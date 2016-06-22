090fff3d77f89c069d077c07fd4db5e8654ae3f6
Anthony Cossins <anthonyecossins@gmail.com>
2016-06-22 01:14:28 +0100

    Added basic TCP client with byte stream reading

--- printer/main.ino
+++ printer/main.ino
@@ -1,21 +1,42 @@
-// Need this empty line to stop big errors hmmm
-// Credit: https://github.com/atheros/adafruit_thermal_sparkcore
-#include "Adafruit_Thermal.h"
 
-Adafruit_Thermal printer;
+TCPClient client;
+// byte server[] = { 192, 168, 55, 97 };
+byte server[] = { 192, 168, 1, 70 };
 
 void setup() {
-  Serial1.begin(19200);
-  printer.begin(&Serial1);
+  // Connect to USB serial
+  Serial.begin(9600);
+  Serial.println("USB connected");
+  Particle.function("requestData", requestData);
 
-  // Expose printLine() function to Particle cloud endpoint
-  Particle.function("printLine", printLine);
+  uint32_t freemem = System.freeMemory();
+  Serial.print("Free memory: ");
+  Serial.println(freemem);
+
+  size_t length = EEPROM.length();
+  Serial.print("EEPROM length: ");
+  Serial.println(length);
 }
 
-void loop() {}
+void loop() {
+  if (client.available()) {
+    byte c = client.read();
+    Serial.println(c);
+  }
 
-int printLine(String text) {
-  printer.println(text);
-  // printer.feed(2);
+  if (!client.connected()) {
+    client.stop();
+  }
+}
+
+int requestData(String url) {
+  Serial.println("Requesting data");
+
+  if (client.connect(server, 2000)) {
+    Serial.println("Client connected!!!");
+  } else {
+    Serial.println("Client failed to connect");
+  }
+
   return 1;
 }

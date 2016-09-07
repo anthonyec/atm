3c1e41973e0453f8af640a02f99ad02a7a5a2922
Anthony Cossins <anthonyecossins@gmail.com>
2016-09-07 09:30:56 +0100

    WIP - Sequalize

--- backend/models/robot.js
+++ backend/models/robot.js
@@ -2,16 +2,9 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Robot = sequelize.define('Robot', {
-    postcode: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: 'incomplete',
-    },
-    prediction: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
+    name: DataTypes.STRING,
+    photonId: DataTypes.STRING,
+    photonName: DataTypes.STRING,
   }, {
     classMethods: {
       associate: function(models) {
--- backend/routes/test.js
+++ backend/routes/test.js
@@ -3,13 +3,46 @@ const models  = require('../models');
 
 const router  = express.Router();
 
+// models.Robot.create({
+//   name: 'cop',
+//   photonId: '3e0035000347343339373536',
+//   photonName: 'red',
+// });
+
+// models.Robot.create({
+//   name: 'doctor',
+//   photonId: '',
+//   photonName: '',
+// });
+
+// models.Robot.create({
+//   name: 'money',
+//   photonId: '',
+//   photonName: '',
+// });
+
+// models.Robot.create({
+//   name: 'prof',
+//   photonId: '',
+//   photonName: '',
+// });
+
+// models.Robot.findOne({
+//   where: {
+//     name: 'red',
+//   },
+// }).then((results) => {
+//   console.log(results);
+// });
+
 router.get('/', function(req, res) {
-  models.Request.create({
-    postcode: 'SE20 8PJ',
-    phoneNumber: '+440000000000',
-  }).then(function() {
-    console.log('wow');
-  });
+  // models.Request.create({
+  //   postcode: 'SE20 8PJ',
+  //   phoneNumber: '+440000000000',
+  // }).then(function() {
+  //   console.log('wow');
+  // });
+
 
   res.sendStatus(200);
 });

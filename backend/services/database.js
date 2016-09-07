6304e9251af81c9e8ff8a6d16784a7dde6e24e66
Anthony Cossins <anthonyecossins@gmail.com>
2016-09-07 22:18:37 +0100

    Moved tcp and database to services

--- backend/services/database.js
+++ backend/services/database.js
@@ -1,10 +1,8 @@
-'use strict';
-
-// http://tinyurl.com/zx7b62v
-
-const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV]);
+const env = process.env.NODE_ENV;
+const knex = require('knex')(require('..//knexfile')[env]);
 const bookshelf = require('bookshelf')(knex);
 
+// http://tinyurl.com/zx7b62v
 bookshelf.plugin('registry'); // Resolve circular dependencies with relations
 
 module.exports = bookshelf;

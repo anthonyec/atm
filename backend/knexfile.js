5d89801650ff5e2429ec0048cbd5f0d6e400aeb1
Zdenek Hynek <zdenek@signal-noise.co.uk>
2016-09-12 18:56:35 +0100

    Added production knex config

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -8,4 +8,13 @@ module.exports = {
       database: 'pyb_robots',
     },
   },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOSTNAME || '',
+      user: process.env.DB_USERNAME || '',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || '',
+    }
+  }
 };

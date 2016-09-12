e0e63b6e549f714671148aca22c3f3815d7be32d
Zdenek Hynek <zdenek@signal-noise.co.uk>
2016-09-12 20:41:14 +0100

    Installed relic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['pyb-atm-app'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '6170fe1cd9414ad5ce939d91532562a760041711',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}

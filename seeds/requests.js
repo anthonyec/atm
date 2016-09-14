1e4d98cf52eb67d4c84149edf13bb2e3129c2bba
Zdenek Hynek <zdenek@signal-noise.co.uk>
2016-09-14 22:09:32 +0100

    Remove testing requests from the seed

--- seeds/requests.js
+++ seeds/requests.js
@@ -1,33 +1,6 @@
 const consts = require('../models/request').consts;
 
-const requests = [
-  {
-    postcode: 'se26',
-    status: consts.COMPLETE,
-    phoneNumber: '+440000000000',
-    robotId: 2,
-  },
-  {
-    postcode: 'se20 8pj',
-    phoneNumber: '+440000000002',
-    robotId: 1,
-  },
-  {
-    postcode: 'SW15 2QS',
-    phoneNumber: '+440000000003',
-    robotId: 3,
-  },
-  {
-    postcode: 'EC2',
-    phoneNumber: '+440000000004',
-    robotId: 2,
-  },
-  {
-    postcode: 'EC2',
-    phoneNumber: '+440000000005',
-    robotId: 4,
-  },
-];
+const requests = [];
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
